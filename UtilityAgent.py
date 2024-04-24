98d61b121bc1a7c524825938c88d074d388cd163
Ashkan Nikfarjam <ashkan_nikfarjam@yahoo.com>
2024-04-24 15:31:21 -0700

    first commit ANikfarjam

---
+++ UtilityAgent.py
@@ -0,0 +1,6 @@
+class UtilityAgent:
+    def __init__(self, board, Player, Agent):
+        self.board = board 
+        self.player = Player #state of players peaces
+        self.agent =Agent #state od Agent peaces
+    
