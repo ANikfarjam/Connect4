103fa27fc0488a8f4c98cff78e85b0a4806fe801
AlexTeixi <147417367+AlexTeixi@users.noreply.github.com>
2024-04-24 15:49:44 -0700

    Add files via upload

--- UtilityAgent.py
+++ UtilityAgent.py
@@ -1,6 +1,6 @@
 class UtilityAgent:
     def __init__(self, board, Player, Agent):
         self.board = board 
-        self.player = Player #state of players peaces
-        self.agent =Agent #state od Agent peaces
+        self.player = Player #state of players pieces
+        self.agent =Agent #state od Agent pieces
     
