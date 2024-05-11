b3c2c7813bd390d873c8b4810e11f8b17530b835
Ashkan Nikfarjam <ashkan_nikfarjam@yahoo.com>
2024-05-11 16:07:31 -0700

    rebasing

--- UtilityAgent.py
+++ UtilityAgent.py
@@ -4,3 +4,16 @@ class UtilityAgent:
         self.player = Player #state of players pieces
         self.agent =Agent #state od Agent pieces
     
+class Node:
+    def __init__(self, value = None, childeren=[]):
+        self.max_value = value
+        self.child = childeren
+    def add_Node(self, Node):
+        self.child.append(Node)
+    def get_value(self):
+        return self.max_value
+def apha_beta_search(board, depth_limit, ):
+    pass
+def construct_tree(board):#new board state/return index of witch column we add the next peace
+    pass
+
